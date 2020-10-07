de87adbeeb5d71189f877140060d1eabb39ee902
Bartosz Niziol <vxcx940@gmail.com>
2020-10-07 12:12:49 +0200

    Contact - Customer - Adress connection initialized and tested

--- src/test/java/pl/practic/shirtshop/entity/ContactsEntityTest.java
+++ src/test/java/pl/practic/shirtshop/entity/ContactsEntityTest.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import pl.practic.shirtshop.entities.Contacts;
+import pl.practic.shirtshop.entities.Customer;
 import pl.practic.shirtshop.repositories.ContactsRepository;
+import pl.practic.shirtshop.repositories.CustomerRepository;
 
 import javax.transaction.Transactional;
 
@@ -19,6 +21,9 @@ public class ContactsEntityTest {
 
     @Autowired
     ContactsRepository contactsRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
 
     @Test
     @Transactional
@@ -37,5 +42,44 @@ public class ContactsEntityTest {
         Assert.assertEquals(phoneNumber1, contactsPulled.getPhoneNumber1());
     }
 
+    @Test
+    @Transactional
+    public void shouldAddContactToCustomerAndGetDirectlyFromDB_PhoneNumber2EmailCheck(){
+        //given
+        String phoneNumber ="123456789";
+        String email ="auuu@ww.pl";
+        Contacts contacts = new Contacts();
+        contacts.setEmail(email);
+        contacts.setPhoneNumber2(phoneNumber);
+
+        Customer customer = new Customer();
+        customer.setContacts(contacts);
+        //when
+        customerRepository.save(customer);
+        Contacts contacts2 = contactsRepository.getOne(contacts.getId());
+        //then
+        Assert.assertEquals(phoneNumber,contacts2.getPhoneNumber2());
+        Assert.assertEquals(email,contacts2.getEmail());
+
+    }
+
+    @Test
+    @Transactional
+    public void shouldAddAndGetContactFromCustomer_FaxCheck(){
+        //given
+        String fax ="123456789";
+        Contacts contacts = new Contacts();
+        contacts.setFax(fax);
+
+        Customer customer = new Customer();
+        customer.setContacts(contacts);
+        //when
+        customerRepository.save(customer);
+        Customer customer2 = customerRepository.getOne(customer.getId());
+        //then
+        Assert.assertEquals(fax,customer2.getContacts().getFax());
+
+
+    }
 
 }
--- src/test/java/pl/practic/shirtshop/entity/CustomerEntityTest.java
+++ src/test/java/pl/practic/shirtshop/entity/CustomerEntityTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import pl.practic.shirtshop.entities.Adress;
+import pl.practic.shirtshop.entities.Contacts;
 import pl.practic.shirtshop.entities.Customer;
 
 import pl.practic.shirtshop.repositories.CustomerRepository;
@@ -61,6 +62,75 @@ public class CustomerEntityTest {
         //then
         Assert.assertEquals(customer2.getFirstName(),firstName);
         Assert.assertEquals(customer2.getLastName(),lastName);
+
+    }
+
+    @Test
+    public void shouldAddAndGetOneCustomerWithContacts_ConnectionTest_FirstNameLastNameCheck() {
+        //given
+        Customer customer = new Customer();
+        Contacts contacts = new Contacts();
+        String firstName = "Harry";
+        String lastName = "Kane";
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        customer.setContacts(contacts);
+        //when
+        customerRepository.save(customer);
+        Optional<Customer> customerOptional =  customerRepository.findById(customer.getId());
+        Customer customer2 = customerOptional.get();
+        //then
+        Assert.assertEquals(customer2.getFirstName(),firstName);
+        Assert.assertEquals(customer2.getLastName(),lastName);
+
+    }
+
+    @Test
+    public void shouldAddAndGetOneCustomerWithContactsAndAdress_ConnectionTest_FirstNameLastNameCheck() {
+        //given
+        Customer customer = new Customer();
+        Contacts contacts = new Contacts();
+        Adress adress = new Adress();
+        String firstName = "Harry";
+        String lastName = "Kane";
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        customer.setContacts(contacts);
+        customer.setAdress(adress);
+        //when
+        customerRepository.save(customer);
+        Optional<Customer> customerOptional =  customerRepository.findById(customer.getId());
+        Customer customer2 = customerOptional.get();
+        //then
+        Assert.assertEquals(customer2.getFirstName(),firstName);
+        Assert.assertEquals(customer2.getLastName(),lastName);
+
+    }
+    @Test
+    public void shouldAddAndGetOneCustomerWithContactsAndAdress_CompatibilityTest() {
+        //given
+        Customer customer = new Customer();
+        Contacts contacts = new Contacts();
+        Adress adress = new Adress();
+        String firstName = "Harry";
+        String lastName = "Kane";
+        String phoneNumber = "123456789";
+        String street ="Moidowa";
+        contacts.setPhoneNumber1(phoneNumber);
+        adress.setStreet(street);
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        customer.setContacts(contacts);
+        customer.setAdress(adress);
+        //when
+        customerRepository.save(customer);
+        Optional<Customer> customerOptional =  customerRepository.findById(customer.getId());
+        Customer customer2 = customerOptional.get();
+        //then
+        Assert.assertEquals(customer2.getFirstName(),firstName);
+        Assert.assertEquals(customer2.getLastName(),lastName);
+        Assert.assertEquals(phoneNumber,customer2.getContacts().getPhoneNumber1());
+        Assert.assertEquals(street,customer2.getAdress().getStreet());
 
     }
 }
